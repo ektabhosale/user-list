c79dbf10c669cc32a421a5fd17149c26e8af4521
EktaLinux <ebhosale73@hmail.com>
2020-09-19 13:04:45 +0530

    removed fetch file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import FetchApi from './FetchApi'
 import * as serviceWorker from './serviceWorker';
 import MainComp from './components/MainComp'
 
