767daf47fc7bf613438e132353428036ab91dd32
EktaLinux <ebhosale73@hmail.com>
2020-09-19 13:02:45 +0530

    Updates

---
+++ precache-manifest.9efd1040a3ab7bf671c7fc38d5d9cdf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "025a3d8d4ce8e09eaa519329efea17a0",
+    "url": "/user-list/index.html"
+  },
+  {
+    "revision": "132ee36a81f7d5c81685",
+    "url": "/user-list/static/css/main.402fbb8c.chunk.css"
+  },
+  {
+    "revision": "d06f3be8f3f1cc83dbca",
+    "url": "/user-list/static/js/2.613c7806.chunk.js"
+  },
+  {
+    "revision": "531d7b2065717e21d8dbdd1394bf24a4",
+    "url": "/user-list/static/js/2.613c7806.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "132ee36a81f7d5c81685",
+    "url": "/user-list/static/js/main.c825b228.chunk.js"
+  },
+  {
+    "revision": "8bd4be28689c9e23a386",
+    "url": "/user-list/static/js/runtime-main.f652996a.js"
+  }
+]);
